998516db1813a17b865293bbee713738202bbf8a
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-12-12 12:43:12 +0530

    first commit

---
+++ Self_Join_Example.sql
@@ -0,0 +1,14 @@
+create table tbl_EMP(Id int , Name varchar(10),Manager int)
+
+insert into tbl_EMP values(1,'adi',0)
+insert into tbl_EMP values(2,'anil',1)
+insert into tbl_EMP values(3,'vani',2)
+insert into tbl_EMP values(4,'abc',null)
+
+select * from tbl_EMP
+
+select E.Id,E.Name,M.Name as Manage from tbl_EMP E left outer join tbl_EMP M on E.Manager=M.Id
+
+--select E.Id,E.Name,M.Name as Manage from tbl_EMP E  join tbl_EMP M on E.Manager=M.Id
+
+--select E.Id,E.Name,M.Name as Manage from tbl_EMP E , tbl_EMP M where E.Manager=M.Id
